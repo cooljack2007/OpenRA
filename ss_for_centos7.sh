654a05fe79f53a46f9effb01c13f0ee86f215e8d
cooljack2007 <303739912@qq.com>
2019-03-14 18:14:57 +0800

    Update ss_for_centos7.sh

--- ss_for_centos7.sh
+++ ss_for_centos7.sh
@@ -38,7 +38,7 @@ systemctl restart shadowsocks-server
 
 
 yum install -y wget
-VERSION=20190109
+VERSION=20181114
 wget https://github.com/xtaci/kcptun/releases/download/v$VERSION/kcptun-linux-amd64-$VERSION.tar.gz
 tar zxf kcptun-linux-amd64-$VERSION.tar.gz
 rm -f client_linux_amd64 kcptun-linux-amd64-$VERSION.tar.gz
